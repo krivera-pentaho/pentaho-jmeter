6d856e6d8f4faaafe646e769d0e6672256b80934
Kris Rivera <krivera@pentaho.com>
2014-07-23 11:41:46 -0400

    Added test class to start test-src

--- test-src/org/pentaho/jmeter/MainTest.java
+++ test-src/org/pentaho/jmeter/MainTest.java
@@ -1,4 +1,18 @@
 package org.pentaho.jmeter;
 
+import org.junit.Before;
+import org.junit.Test;
+
+
 public class MainTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void testMain() {
+
+    }
 }
